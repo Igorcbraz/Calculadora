e3f383878f03166491fa12b9fe0b2f1cb84990df
Igor Costa Braz <Igorcbraz1@gmail.com>
2021-07-14 19:41:51 -0300

    Make te code more clean

--- script.js
+++ script.js
@@ -39,70 +39,10 @@ function del(){
     =================== TOGGLE THEME ===================
     ====================================================
 */
-// Get the root element
-var root = document.querySelector(':root');
 
-const darkThemeMq  = window.matchMedia("(prefers-color-scheme: dark)");
-const lightThemeMq = window.matchMedia("(prefers-color-scheme: light)");
-
-if (darkThemeMq.matches) {
-    document.getElementById('btnTheme').value = "3";
-
-    root.style.setProperty('--background'          , '#17062a');
-    root.style.setProperty('--background-dark'     , '#1e0836');
-    root.style.setProperty('--background-very-dark', '#1e0836');
-    
-    root.style.setProperty('--key-color-top'       , '#f7de43');
-    root.style.setProperty('--key-color-bottom'    , '#f7de43');
-    root.style.setProperty('--key-background'      , '#331b4d');
-    root.style.setProperty('--key-shadow'          , '#851c9c');
-
-    root.style.setProperty('--key-blue-background' , '#56077c');
-    root.style.setProperty('--key-blue-shadow'     , '#851c9c');
-
-    root.style.setProperty('--key-red-background'  , '#00decf');
-    root.style.setProperty('--key-red-shadow'      , '#00decf');
-} else if(lightThemeMq.matches){
-    document.getElementById('btnTheme').value = "2";
-    root.style.setProperty('--background'          , '#e6e6e6');
-    root.style.setProperty('--background-dark'     , '#d3cdcd');
-    root.style.setProperty('--background-very-dark', '#eeeeee');
-    
-    root.style.setProperty('--key-color-top'       , '#3d3d33');
-    root.style.setProperty('--key-color-bottom'    , '#3d3d33');
-    root.style.setProperty('--key-background'      , '#e5e4e0');
-    root.style.setProperty('--key-background-dark' , '#dfd9d2');
-    root.style.setProperty('--key-shadow'          , '#b4a597');
-
-    root.style.setProperty('--key-blue-background' , '#388187');
-    root.style.setProperty('--key-blue-shadow'     , '#1c6166');
-
-    root.style.setProperty('--key-red-background'  , '#d03f2f');
-    root.style.setProperty('--key-red-shadow'      , '#93261a');    
-} else {
-    root.style.setProperty('--background'          , '#3a4764');
-    root.style.setProperty('--background-dark'     , '#232c43');
-    root.style.setProperty('--background-very-dark', '#182034');
-    
-    root.style.setProperty('--key-color-top'       , '#ffffff');
-    root.style.setProperty('--key-color-bottom'    , '#3a4764');
-    root.style.setProperty('--key-background'      , '#eae3dc');
-    root.style.setProperty('--key-background-dark' , '#dfd9d2');
-    root.style.setProperty('--key-shadow'          , '#b4a597');
-
-    root.style.setProperty('--key-blue-background' , '#637097');
-    root.style.setProperty('--key-blue-shadow'     , '#404e72');
-
-    root.style.setProperty('--key-red-background'  , '#d03f2f');
-    root.style.setProperty('--key-red-shadow'      , '#93261a');    
-}
-
-// Create a function for recive the value of range input
-function myFunction_set(val) {
-    document.getElementById('btnTheme').value = val; 
-
-    // Default theme 
-    if(val == 1){
+// All colors for differents themes
+const theme = {
+    defaul(){
         root.style.setProperty('--background'          , '#3a4764');
         root.style.setProperty('--background-dark'     , '#232c43');
         root.style.setProperty('--background-very-dark', '#182034');
@@ -118,10 +58,8 @@ function myFunction_set(val) {
 
         root.style.setProperty('--key-red-background'  , '#d03f2f');
         root.style.setProperty('--key-red-shadow'      , '#93261a');
-    } 
-     
-    // White theme
-    else if(val == 2){
+    },
+    light(){
         root.style.setProperty('--background'          , '#e6e6e6');
         root.style.setProperty('--background-dark'     , '#d3cdcd');
         root.style.setProperty('--background-very-dark', '#eeeeee');
@@ -137,10 +75,8 @@ function myFunction_set(val) {
 
         root.style.setProperty('--key-red-background'  , '#d03f2f');
         root.style.setProperty('--key-red-shadow'      , '#93261a');
-    }
-    
-    // Dark theme
-    else{
+    },
+    dark(){
         root.style.setProperty('--background'          , '#17062a');
         root.style.setProperty('--background-dark'     , '#1e0836');
         root.style.setProperty('--background-very-dark', '#1e0836');
@@ -155,6 +91,46 @@ function myFunction_set(val) {
 
         root.style.setProperty('--key-red-background'  , '#00decf');
         root.style.setProperty('--key-red-shadow'      , '#00decf');
+    }
+}
+
+// Get the root element
+var root = document.querySelector(':root');
+
+// Checking the user themes preference
+const darkThemeMq  = window.matchMedia("(prefers-color-scheme: dark)");
+const lightThemeMq = window.matchMedia("(prefers-color-scheme: light)");
+
+// changing the theme with the results above
+if (darkThemeMq.matches) {
+    document.getElementById('btnTheme').value = "3";
+    theme.dark();
+} else if(lightThemeMq.matches){
+    document.getElementById('btnTheme').value = "2";
+    theme.light();    
+} else {
+    document.getElementById('btnTheme').value = "1";
+    theme.defaul();    
+}
+
+// Create a function for recive the value of range input
+function myFunction_set(val) {
+    // receiving the input type range value
+    document.getElementById('btnTheme').value = val; 
+
+    // changing the theme with the results above
+    if(val == 1){
+        theme.defaul();
+    } 
+     
+    else if(val == 2){
+        theme.light();
+    }
+    
+    else{
+        theme.dark();
     } 
   
 }
+
+
