efb342913c7279315262222529f3a1dcaae55a41
Igor <Igorcbraz1@gmail.com>
2024-11-27 20:22:29 -0300

    feat: add github stars counter

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,9 @@
 import { themes } from './themes.js'
 import { buttons } from './buttons.js'
+import { getStarsUrl } from './getStarsUrl.js'
 
 export {
   themes,
-  buttons
+  buttons,
+  getStarsUrl
 }
